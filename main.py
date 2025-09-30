037d624199d4dd7bc523a326987aa86545856756
ksvenson <kai.svenson628@gmail.com>
2025-09-30 01:50:01 -0500

    plotting full syk spectrum

--- main.py
+++ main.py
@@ -2,7 +2,23 @@ import numpy as np
 import scipy as sp
 import matplotlib.pyplot as plt
 
-import 
+import SYK_Full as syk
 
 if __name__ == "__main__":
-    print('Hello World!')
+    seed = 408139579
+
+    N = 20
+    q = 4
+
+    H = syk.Hamil(N, q, seed)
+    print(H.shape)
+
+    evals = H.eigenenergies()
+    np.save(f'full_syk_evals_N{N}_q{q}.npy', evals)
+
+    fig, ax = plt.subplots()
+
+    ax.hist(evals, bins=100)
+    ax.set(xlabel='Energy', ylabel='Counts', title=rf'$N={N}$, $q={q}$, Full SYK Spectrum')
+
+    fig.savefig('spectrum.svg', bbox_inches='tight')
